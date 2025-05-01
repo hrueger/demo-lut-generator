c8a2bc8b076009a29fb4c3337173f64f3ee104e6
Hannes Rüger <hannes@rueger.events>
2025-05-01 10:01:47 +0200

    feat: basic 1d lut generator

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import "bootstrap/dist/css/bootstrap.min.css";
+    import { onMount } from "svelte";
+    onMount(async () => {
+        // @ts-expect-error
+        await import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    });
+</script>
+
+<div class="container mt-3">
+    <h1 class="border-bottom mb-3">Demo LUT Generator</h1>
+    <slot></slot>
+</div>
